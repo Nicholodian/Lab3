03ede422d6c0ee5b11b36c40d5cf31c598defa83
Nic <nicholasl77.22@ichat.sp.edu.sg>
2023-11-04 21:27:23 +0800

    added everything as of now

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,23 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_more_than_ten():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 9, 17, 45, 86]
+    test_result = 1
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert (result == test_result)
+def test_bubble_empty():
+    result = []
+    input_arr = []
+    test_result = 0
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert(result == test_result)
+
+def test_bubble_non_int():
+   result = []
+   input_arr = ["a", 2, 3]
+   test_result = 2
+   result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+   assert(result == test_result)
